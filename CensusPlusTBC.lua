9fec1c70e74a0599f0cd1e24d1c56ea2a8dbca38
Jacob Bowen <jacob@gamingrepublic.net>
2021-11-05 12:45:56 -0600

    Bumped version so it will actually work with the website.

--- CensusPlusTBC.lua
+++ CensusPlusTBC.lua
@@ -54,8 +54,8 @@ BINDING_HEADER_CENSUSPLUSTBC = 'CensusPlusTBC'
 -- Constants
 local CensusPlus_Version_Major = "0"; -- changing this number will force a saved data purge
 local CensusPlus_Version_Minor = "8"; -- changing this number will force a saved data purge
-local CensusPlus_Version_Maint = "8";
-local CensusPlus_SubVersion = "2";
+local CensusPlus_Version_Maint = "9";
+--local CensusPlus_SubVersion = "2";
 local CensusPlus_VERSION = CensusPlus_Version_Major.."."..CensusPlus_Version_Minor .."."..CensusPlus_Version_Maint .."."..CensusPlus_SubVersion;
 local CensusPlus_VERSION_FULL = CensusPlus_VERSION --.."."..CensusPlus_SubVersion ;
 local CensusPlus_PTR = GetCVar("portal") == "public-test" and "PTR";	-- enable true for PTR testing  enable false for live use
